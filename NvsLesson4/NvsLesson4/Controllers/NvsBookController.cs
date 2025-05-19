eb8d23e4393f1333c5a5baa9368689e697f1fe83
lmeooo-231 <nvson2311@gmail.com>
2025-05-19 20:47:13 +0700

    lesson4

---
+++ NvsLesson4/NvsLesson4/Controllers/NvsBookController.cs
@@ -0,0 +1,19 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using NvsLesson04.Models;
+
+namespace NvsLesson04.Controllers
+{
+    public class NvsBookController : Controller
+    {
+        protected Book book = new Book();
+        public IActionResult NvsIndex()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+
+            var books = book.GetBookList();
+            return View(books);
+        }
+    }
+}
