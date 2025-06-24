d62fcae14701edc122ce984d67fc5e9375166798
lmeooo-231 <nvson2311@gmail.com>
2025-06-24 11:34:11 +0700

    lesson10

---
+++ nvslesson10/nvslesson10/Models/NvsCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nvslesson10.Models;
+
+public partial class NvsCategory
+{
+    public int CateId { get; set; }
+
+    public string? CateName { get; set; }
+
+    public bool? CateStatus { get; set; }
+}
---
+++ nvslesson10/nvslesson10/Models/NvsLesson10dbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace nvslesson10.Models;
+
+public partial class NvsLesson10dbContext : DbContext
+{
+    public NvsLesson10dbContext()
+    {
+    }
+
+    public NvsLesson10dbContext(DbContextOptions<NvsLesson10dbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<NvsCategory> Categories { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=USER\\SQLEXPRESS;Database=NvsLesson10db;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NvsCategory>(entity =>
+        {
+            entity.HasKey(e => e.CateId);
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CateId)
+                .ValueGeneratedNever()
+                .HasColumnName("CateID");
+            entity.Property(e => e.CateName).HasMaxLength(150);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
